ba9b30d842ab1729a7e662168a114297cdfc6d71
Isaías Ramos <isaias.ramos.olive@gmail.com>
2023-07-28 08:55:00 -0300

    schemas created

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,9 @@
 import express from "express"
+import { router } from "./routes/route";
+const app = express();
+app.use(express.json());
+app.use(router)
+const port = 3001 || process.env.PORT
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+})
